5a234c5334407bc9fe1b6cd4f4567e4a238d7079
Aminu Hamza Nababa <70522655+Alamein@users.noreply.github.com>
2026-01-22 18:53:35 +0100

    'update'

---
+++ python_crs_cs/list/pizza.py
@@ -0,0 +1,6 @@
+pizzas = ['almon-bada', 'pinot-bada', 'pizza']
+
+for pizza in pizzas:
+    print('I like ' + pizza.title() + ', Pizza')
+
+print('\nI really like to eat Pizza.\nExpecially ' + pizzas[1].title() + '.')
