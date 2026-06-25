b06dc22c396d773ea3cf60bd92ba7ae36ee564a9
Alamein <alaminnababa@gmail.com>
2026-06-25 15:42:13 +0100

    updated a class

--- ProjectsFolder/classes/car.py
+++ ProjectsFolder/classes/car.py
@@ -49,6 +49,15 @@ class Battery():
         '''Print a statement describing the battery size."'''
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        
+        message = f'This car can go approximately {str(range)}'
+        message += ' miles on a full charge.'
+        print(message)
 
 class ElectricCar(Car):
     def __init__(self, make, model, year):
