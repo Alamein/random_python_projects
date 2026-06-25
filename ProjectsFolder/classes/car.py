1f769e64a1ee520c50417a692b6757515c5d3de3
Engr. Al-amin Nababa <alaminhnab4@gmail.com>
2026-06-25 15:31:48 +0100

    update

--- ProjectsFolder/classes/car.py
+++ ProjectsFolder/classes/car.py
@@ -38,3 +38,30 @@ my_car.odometer_reading = 32
 # my_car.update_odometer(21)
 my_car.increment_odometer(20)
 my_car.read_odometer()
+
+
+class Battery():
+    def __init__(self, battery_size = 70):
+        '''A simple attempt to model a battery for an electric car.'''
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''Print a statement describing the battery size."'''
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        '''Initialize attributes of the parent class.'''
+        super().__init__(make, model, year)
+
+        self.battery = Battery()
+
+    # def describe_battery(self):
+    #     '''Print a statement describing the battery size.'''
+    #     print(f'This has a {str(self.batterty_size)}-kWh battery.')
+
+my_tesla = ElectricCar('tesla', 'model s', 2025)
+
+print(my_tesla.get_descriptive_name())
+print(my_tesla.battery.describe_battery())
