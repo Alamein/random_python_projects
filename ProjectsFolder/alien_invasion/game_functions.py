88057897501f547df8aa6a849cd3715e0a492ff0
Alamein <alaminhnab4@gmail.com>
2026-07-12 01:13:00 +0100

    update

--- ProjectsFolder/alien_invasion/game_functions.py
+++ ProjectsFolder/alien_invasion/game_functions.py
@@ -2,6 +2,22 @@ import sys
 
 import pygame
 
+def check_keydown_events(event, ship):
+    '''Respond to keypresses.'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    '''Respond to key releases.'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     '''Respond to keypresses and mouse events.'''
     for event in pygame.event.get():
@@ -9,18 +25,21 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
+            check_keydown_events(event, ship)
+            # if event.key == pygame.K_RIGHT:
                 # Move the ship to the right.
                 # ship.rect.centerx += 1
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+                # ship.moving_right = True
+
+            # elif event.key == pygame.K_LEFT:
+                # ship.moving_left = True
         
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+            # if event.key == pygame.K_RIGHT:
+                # ship.moving_right = False
+            # elif event.key == pygame.K_LEFT:
+                # ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
