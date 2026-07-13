458a6edc439f8e35083a41a3094f459c0eade9f8
Alamein <alaminhnab4@gmail.com>
2026-07-13 14:07:25 +0100

    update

--- ProjectsFolder/alien_invasion/alien_invasion.py
+++ ProjectsFolder/alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 # import sys :already imported in the module game_functions
 
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -18,20 +19,29 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
 
         #  Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         # for event in pygame.event.get():
         #     if event.type == pygame.QUIT:
         #         sys.exit()
 
         ship.update()
+        bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        # print(len(bullets))
 
         # Redraw the screen during each pass through the loop.
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         # screen.fill(ai_settings.bg_color)
         # ship.blitme()
 
